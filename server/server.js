23c3470705f3ffe6b9eb92efa43968e50b694944
Nico Vignola <nico.vignola@traderepublic.com>
2020-04-22 17:46:27 +0200

    Server: support cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
@@ -9,6 +10,7 @@ const mainRoutes = require("./routes/main");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(
