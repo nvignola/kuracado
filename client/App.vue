04e662b348fb86250d417e3acff2ed217f87c562
Nico Vignola <nico.vignola@traderepublic.com>
2020-04-22 10:26:17 +0200

    Initial client configuration

---
+++ client/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">Hello {{title}}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      title: "Kuracado"
+    };
+  }
+});
+</script>
+
+<style scoped>
+.container {
+  color: green;
+}
+</style>
