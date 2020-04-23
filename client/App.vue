c13ea1daed2cae0bc2e3a8a13b467bc3a439cd27
Nico Vignola <nico.vignola@traderepublic.com>
2020-04-23 19:24:50 +0200

    App: add new components to main view

--- client/App.vue
+++ client/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="container">Hello {{title}}</div>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <patients-table></patients-table>
+    <receipt-form></receipt-form>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import PatientsTable from "./components/PatientsTable.vue";
+import ReceiptForm from "./components/ReceiptForm.vue";
 
 export default Vue.extend({
+  name: "Kuracado",
+  components: {
+    "patients-table": PatientsTable,
+    "receipt-form": ReceiptForm,
+  },
   data() {
     return {
-      title: "Kuracado"
+      title: "Kuracado 💊",
     };
-  }
+  },
 });
 </script>
 
-<style scoped>
-.container {
-  color: green;
-}
-</style>
+<style scoped></style>
