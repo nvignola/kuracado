f0ed23d67442259a86476e64f24fd0e81550c5df
Nico Vignola <nico.vignola@traderepublic.com>
2020-04-23 19:25:29 +0200

    Index: add bootstrap components

--- client/index.js
+++ client/index.js
@@ -1,4 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({ render: (createElement) => createElement(App) }).$mount("#app");
