ca2125fd0ab37322d07dcfcda65803898877b6a5
Nico Vignola <nico.vignola@traderepublic.com>
2020-04-25 15:22:27 +0200

    Receipt: add drugs list to create function param

--- server/utils/createReceipt.js
+++ server/utils/createReceipt.js
@@ -2,16 +2,17 @@ const nodeHtmlToImage = require("node-html-to-image");
 const receiptTemplate = require("../templates/receipt");
 
 module.exports = (receiptData) => {
-  const { patientFullname, patientInsuranceID } = receiptData;
+  const { patientFullname, patientInsuranceID, medications } = receiptData;
 
   return nodeHtmlToImage({
-    output: `.server/receipts/receipt_${patientInsuranceID}.png`,
+    output: `public/receipt_${patientInsuranceID}.png`,
     html: receiptTemplate,
     content: {
       createdAt: new Date().toLocaleDateString(),
       dueTo: new Date().toLocaleDateString(),
       patientFullname,
       patientInsuranceID,
+      medications,
     },
   });
 };
