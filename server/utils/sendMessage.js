5fcf93561d7332c719e72165ecf6a1c72134b9c3
Nico Vignola <nico.vignola@traderepublic.com>
2020-04-25 15:21:58 +0200

    Add media url to send message params

--- server/utils/sendMessage.js
+++ server/utils/sendMessage.js
@@ -3,9 +3,10 @@ const cfg = require("../config");
 
 const client = twilio(cfg.twilioAccountSid, cfg.twilioAuthToken);
 
-module.exports = (to, body) =>
+module.exports = (to, body, mediaUrl) =>
   client.messages.create({
     from: `whatsapp:${cfg.twilioPhoneNumber}`,
     body,
     to,
+    ...(mediaUrl && { mediaUrl }),
   });
