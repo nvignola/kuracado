f589f76c01ed27a453a7b78982dff533e702f99d
Nico Vignola <nico.vignola@traderepublic.com>
2020-04-26 16:54:36 +0200

    Server: let patient know when packed shipped

--- server/routes/main.js
+++ server/routes/main.js
@@ -8,6 +8,7 @@ const cfg = require("../config");
 const router = express.Router();
 
 const messages = {};
+const msgQueue = [];
 
 // GET: /all-messages
 router.get("/all-messages", (req, res, next) => {
@@ -16,24 +17,29 @@ router.get("/all-messages", (req, res, next) => {
 
 // POST: /send-message
 router.post("/send-message", async (req, res, next) => {
+  const { to, body } = req.body;
+  const patient = messages[to];
   const params = {
     patientFullname: req.body.patientFullname,
     patientInsuranceID: req.body.patientInsuranceID,
     medications: req.body.medications,
+    address: patient.address,
   };
 
   const fileName = await createReceipt(params);
-  const { to, body } = req.body;
   const receiptUrl = `${cfg.serverUrl}/${fileName}`;
+  const pharmacyText = `Please, once the medicine are shipped send the tracking number.`;
 
-  messages[fromWeb].receiptLink = receiptUrl;
+  patient.receiptLink = receiptUrl;
 
   try {
     const { sid } = await sendMessage(to, body, receiptUrl);
+    await sendMessage(cfg.pharmacyPhoneNumber, pharmacyText, receiptUrl);
     res.send({
       status: "success",
       message: `Message sent to ${to}. Message SID: ${sid}`,
     });
+    msgQueue.push(to);
   } catch (err) {
     console.error(err);
     res.status(500).send({
@@ -44,7 +50,7 @@ router.post("/send-message", async (req, res, next) => {
 });
 
 // POST: /receive-message
-router.post("/receive-message", ({ body }, res) => {
+router.post("/receive-message", async ({ body }, res) => {
   const {
     From: from,
     Body: textMessage,
@@ -54,6 +60,30 @@ router.post("/receive-message", ({ body }, res) => {
 
   const phoneNumberRegExp = /(?<phoneNum>\+?\d{8,})/;
   const [phoneNumber] = from.match(phoneNumberRegExp);
+
+  // Intercept pharmacy answer
+  if (phoneNumber === cfg.pharmacyPhoneNumber) {
+    const to = msgQueue.shift();
+    const body = `*From Pharmacy*: ${textMessage}`;
+
+    messages[to].isPackageSent = true;
+
+    try {
+      const { sid } = await sendMessage(to, body);
+      res.send({
+        status: "success",
+        message: `Message sent to ${to}. Message SID: ${sid}`,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        status: "error",
+        message: "Failed to send message. Check server logs for more details.",
+      });
+    }
+
+    return;
+  }
 
   const geoData = {
     latitude,
@@ -69,9 +99,15 @@ router.post("/receive-message", ({ body }, res) => {
     messages[phoneNumber].messages.push(message);
     messages[phoneNumber] = { ...messages[phoneNumber], ...geoData };
   } else {
+    const address = `${faker.address.streetAddress(
+      "##"
+    )} ${faker.address.city()}`;
+
     const extra = {
       name: faker.name.findName(),
       insuranceId: faker.internet.password(),
+      address,
+      isPackageSent: false,
     };
 
     messages[phoneNumber] = { messages: [message], ...geoData, ...extra };
