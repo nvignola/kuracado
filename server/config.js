cedb62a3965ee8d0ae44618d5fe1f749ec71d4f6
Nico Vignola <nico.vignola@traderepublic.com>
2020-04-26 11:26:41 +0200

    Add server url to config

--- server/config.js
+++ server/config.js
@@ -1,4 +1,3 @@
-const path = require("path");
 require("dotenv-safe").config();
 
 const cfg = {};
@@ -9,8 +8,8 @@ cfg.twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 cfg.twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 cfg.twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 cfg.patientPhoneNumber = process.env.PATIENT_PHONE_NUMBER;
-cfg.doctorPhoneNumber = process.env.DOCTOR_PHONE_NUMBER;
 cfg.pharmacyPhoneNumber = process.env.PHARMACY_PHONE_NUMBER;
+cfg.serverUrl = process.env.SERVER_URL;
 
 // Export configuration object
 module.exports = cfg;
